4b0af5b0410ed74e211dd46e5fb8b6beca4cb4ee
MJ Rossetti (@s2t2) <s2t2mail@gmail.com>
2014-12-07 09:43:42 -0800

    parsing api responses (#7); stubbing test responses (#5)

--- lib/gtfs/data_exchange/version.rb
+++ lib/gtfs/data_exchange/version.rb
@@ -2,6 +2,6 @@ module GTFS
   module DataExchange
 
     # The current gem version. Reference the releases list (https://github.com/databyday/gtfs-data-exchange-api-ruby/releases) for previous releases.
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
